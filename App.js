7d83b3277bb0b01b2c7fdaff308ba58e72505286
JRafael <rafaelmatiashue@gmail.com>
2023-02-14 20:37:32 -0300

    centralizando a coletion

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Switch, Text, View } from 'react-native';
-import { useColorScheme } from 'nativewind';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Switch, Text, View, SafeAreaView } from "react-native";
+import { useColorScheme } from "nativewind";
+import ProductsList from "./components/ProductsLists";
 
 export default function App() {
-  const {colorScheme, toggleColorScheme}=useColorScheme();
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
-      <View className="flex-row w-full gap-5">
-      <Switch value={colorScheme === "dark"} onChange={toggleColorScheme}/>
-      <Text className= "text-2xl font-bold dark:text-white">Nova coleção!</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
+      <View className={"flex-row w-full gap-5"}>
+        <Text className="dark:text-white text-2xl font-bold">
+          New collection
+        </Text>
+        <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
       </View>
-      <StatusBar style={colorScheme === "dark" ? "light": "dark"}/>
-    </View>
+      <ProductsList />
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+    </SafeAreaView>
   );
 }
 
