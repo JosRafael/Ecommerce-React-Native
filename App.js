cbcb96dbcc57df2d79221fb1b8fc9902c55b8f4a
JRafael <rafaelmatiashue@gmail.com>
2023-02-14 20:20:02 -0300

    modo dark

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
 
 export default function App() {
+  const {colorScheme, toggleColorScheme}=useColorScheme();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
+      <Switch value={colorScheme === "dark"} onChange={toggleColorScheme}/>
+      <Text className= "dark:text-white">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
